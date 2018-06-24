b33b6d4c70f5f218c1fc1e600a5dfe7d54ce5177
Milena Tomova <kokolina1888@gmail.com>
2018-06-24 10:08:01 +0300

    sync

---
+++ 09_jQuery_lab/task_01/script.js
@@ -0,0 +1,7 @@
+var images = [
+            ['Title image 1', 'https://images.pexels.com/photos/248797/pexels-photo-248797.jpeg?auto=compress&cs=tinysrgb&h=350'],
+            ['Title image 2', 'https://images.pexels.com/photos/34950/pexels-photo.jpg?auto=compress&cs=tinysrgb&h=350'],
+            ['Title image 3', 'https://images.pexels.com/photos/459225/pexels-photo-459225.jpeg?auto=compress&cs=tinysrgb&h=350'],
+            ['Title image 4', 'https://images.pexels.com/photos/257360/pexels-photo-257360.jpeg?auto=compress&cs=tinysrgb&h=350'],
+            ['Title image 5', 'https://images.pexels.com/photos/257840/pexels-photo-257840.jpeg?auto=compress&cs=tinysrgb&h=350']
+        ];
