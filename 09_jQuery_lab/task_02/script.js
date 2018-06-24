a89bdf2ffe2b7eae144e63f3c5a596c6ad1ca7d6
Milena Tomova <kokolina1888@gmail.com>
2018-06-24 12:13:30 +0300

    sync

--- 09_jQuery_lab/task_02/script.js
+++ 09_jQuery_lab/task_02/script.js
@@ -3,13 +3,17 @@ $('#add').on('click', function(e){
   e.preventDefault();
   participant++;
   var inputEl = $('<input>');
-  inputEl.attr('name','name-'+participant);
-  $('#add').after(inputEl);
-  console.log(participant)
-  console.log(inputEl)
+  inputEl.attr({
+    'type':'text',
+    'name':'name-'+participant
+    });
+  $('#add').parent('p').after(inputEl);
+  console.log(participant);
+  console.log(inputEl);
 });
-$('#send').on('click', function(){
+$('#send').on('click', function(e){
+  e.preventDefault();
   var nameElements = $("input[name^='name']");
 
-  console.log(nameElements)
+  console.log(nameElements);
 });
