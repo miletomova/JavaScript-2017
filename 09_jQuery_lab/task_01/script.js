200bbb6663a02e227e0938282062f6808a4c94eb
Milena Tomova <kokolina1888@gmail.com>
2018-06-24 11:04:50 +0300

    sync

--- 09_jQuery_lab/task_01/script.js
+++ 09_jQuery_lab/task_01/script.js
@@ -4,4 +4,18 @@ var images = [
             ['Title image 3', 'https://images.pexels.com/photos/459225/pexels-photo-459225.jpeg?auto=compress&cs=tinysrgb&h=350'],
             ['Title image 4', 'https://images.pexels.com/photos/257360/pexels-photo-257360.jpeg?auto=compress&cs=tinysrgb&h=350'],
             ['Title image 5', 'https://images.pexels.com/photos/257840/pexels-photo-257840.jpeg?auto=compress&cs=tinysrgb&h=350']
-        ];
+        ], flagLeft = 0, flagRight = 0;
+
+$('#left').on('click', function(){
+  if(!flagLeft){
+  	flagLeft = images.length-1;
+  	console.log($('#slider-image img').attr('src'));
+  	$('#slider-image img').attr('src', images[flagLeft][1]);
+  } else {
+  	console.log(flagLeft)
+  	console.log($('#slider-image img').attr('src'));
+
+  	$('#slider-image img').attr('src', images[flagLeft-1][1]);
+  	flagLeft--
+  }
+});
