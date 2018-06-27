f45b20e2ef5327db9d549092a3330cb9a0d35406
Milena Tomova <kokolina1888@gmail.com>
2018-06-27 22:10:18 +0300

    sync

--- 11_test_prep/08_test_prep/task_03/script.js
+++ 11_test_prep/08_test_prep/task_03/script.js
@@ -1,1 +1,17 @@
+$('#btn').on('click', function(e){	
+	e.preventDefault();
+	
+	if ($('span').length) {
+	$('span').remove();	
+	}
+	
+	var name = $('#name').val();
+	
+	if(!name){
 
+		var spanEl = $('<span></span>').html('Name is required').css('color', 'red');
+		
+		$('#name').parent().append(spanEl);
+		}
+	
+});
