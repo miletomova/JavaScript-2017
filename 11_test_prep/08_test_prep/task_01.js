819003a8d32d646f1aa4e539f5d85899d70674a8
Milena Tomova <kokolina1888@gmail.com>
2018-06-27 19:56:05 +0300

    sync

--- 11_test_prep/08_test_prep/task_01.js
+++ 11_test_prep/08_test_prep/task_01.js
@@ -8,8 +8,36 @@
 // ‘2 “” add “” 1 1 asd 2 “” 3’    	[2 1 3]
 
 function uniq(str){
-	
+	var flag = false, resArr = [], arr, len, i;
+// 1- Функцията получава като параметър - стринг. 
+	if(str){
+		arr = str.split(' ');
+		len = arr.length;
+		for(i = 0; i < len; i++){
+			if(!isNaN(arr[i])){
+				flag = true;
+				if(resArr.indexOf(+arr[i]) === -1){
+					resArr.push(+arr[i]);
+				}
+			}
+		}
+		if(!flag){
+			console.log('The input values must be convertible to a number')
+		}
+		
+	} else {
+		console.log('The input must not be an empty string. ');	
+	}
+
+
+// 4 - връща масив с уникални елементи - числа, indexOf
+	if(resArr){
+		console.log(resArr);
+	}
 }
 
-var str = '0 0 0 0';
+//Функцията ви
+// трябва да работи коректно за посочените по-долу входни данни -
+
+var str = '2 “” add “” 1 1 asd 2 “” 3';
 uniq(str);
